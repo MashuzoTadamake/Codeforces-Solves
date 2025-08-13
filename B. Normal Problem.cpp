4cb2e9a0596fd77b6fbb8c2026d8d1e127528444
Tahiad Mashrafi <167784545+MashuzoTadamake@users.noreply.github.com>
2025-08-13 17:18:09 +0600

    Create B. Normal Problem.cpp

---
+++ B. Normal Problem.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    string s;
+    while(t--){
+        cin>>s;
+        for(int i=s.length()-1;i>=0;i--){
+            if (s[i] == 'p') {
+                cout << "q";
+            } else if (s[i] == 'q') {
+                cout << "p";
+            } else {
+                cout << s[i]; 
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
