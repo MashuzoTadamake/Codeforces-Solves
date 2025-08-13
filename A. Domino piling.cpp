c8006691aa622e5176c88329e590307fab81f5c3
Tahiad Mashrafi <167784545+MashuzoTadamake@users.noreply.github.com>
2025-08-13 16:58:24 +0600

    Create A. Domino piling.cpp

---
+++ A. Domino piling.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+using ll = long long;
+
+void solve() {
+  int m, n;
+  cin >> m >> n;
+  int area = m * n;
+  cout << area / 2 << nl;
+}
+
+signed main() {
+  cin.tie(0)->sync_with_stdio(0);
+  ll tc = 1;
+  // cin >> tc;
+  while (tc--) solve();
+}
