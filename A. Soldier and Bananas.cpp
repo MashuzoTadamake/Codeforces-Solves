aa8acac93990651853f376d762c510b66653ba13
Tahiad Mashrafi <167784545+MashuzoTadamake@users.noreply.github.com>
2025-08-13 16:48:08 +0600

    Create A. Soldier and Bananas.cpp

---
+++ A. Soldier and Bananas.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k,n,w,sum=0;
+    cin>>k>>n>>w;
+    for(int i=0;i<=w;i++){
+        sum=sum+(i*k);
+
+        }
+    if(sum>n){
+        cout<<(sum-n);
+    }
+    else 
+        cout<<0;
+
+}
