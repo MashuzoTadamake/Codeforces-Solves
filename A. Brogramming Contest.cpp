c3da5b26594f12000a662aaadc80cb36e4063a75
Tahiad Mashrafi <167784545+MashuzoTadamake@users.noreply.github.com>
2025-08-13 17:27:30 +0600

    Create A. Brogramming Contest.cpp

---
+++ A. Brogramming Contest.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int sum = 0;
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        for (int i = 1; i < n; i++)
+        {
+            if (s[i] != s[i - 1])
+            {
+                sum++;
+            }
+        }
+        if(s[0]=='1'){
+            sum++;
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
